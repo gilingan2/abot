7bbbdd9dcb4465f2f7915bf328e34598c19ed1a2
gilingan2 <gilinganwatu596@gmail.com>
2022-02-21 09:11:29 -0800

    ad new page

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ const app = express()
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('/'))
 app.use(express.static('/exoclick'))
+app.use(express.static('/exoinpage'))
 app.use(express.static('/adsterra'))
+app.use(express.static('/adstera1'))
 app.use(express.static('/direct'))
+app.use(express.static('/newpop'))
 app.use(express.static('/popcash'))
 
 // http://expressjs.com/en/starter/basic-routing.html
@@ -21,6 +24,14 @@ app.get('/exoclick', (request, response) => {
   // eslint-disable-next-line no-undef
   response.sendFile(path.resolve(__dirname, 'exoclick.html'))
 })
+app.get('/exoinpage', (request, response) => {
+  // eslint-disable-next-line no-undef
+  response.sendFile(path.resolve(__dirname, 'exoinpage.html'))
+})
+app.get('/adstera1', (request, response) => {
+  // eslint-disable-next-line no-undef
+  response.sendFile(path.resolve(__dirname, 'adstera1.html'))
+})
 app.get('/adsterra', (request, response) => {
   // eslint-disable-next-line no-undef
   response.sendFile(path.resolve(__dirname, 'adsterra.html'))
@@ -28,6 +39,10 @@ app.get('/adsterra', (request, response) => {
 app.get('/popcash', (request, response) => {
   // eslint-disable-next-line no-undef
   response.sendFile(path.resolve(__dirname, 'popcash.html'))
+})
+app.get('/newpop', (request, response) => {
+  // eslint-disable-next-line no-undef
+  response.sendFile(path.resolve(__dirname, 'newpop.html'))
 })
 app.get('/direct', (request, response) => {
   // eslint-disable-next-line no-undef
