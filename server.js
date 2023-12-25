72142619fa48b95d88e3f84fdb4153f4b7359f73
gilingan2 <99949737+gilingan2@users.noreply.github.com>
2023-12-25 08:42:47 +0000

    add new ads link

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const path = require('path');
+const path = require('path')
 
 const PORT = 9090
 
@@ -7,27 +7,16 @@ const app = express()
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('/'))
-app.use(express.static('/exoclick'))
-app.use(express.static('/exoinpage'))
 app.use(express.static('/adsterra'))
 app.use(express.static('/adstera1'))
 app.use(express.static('/direct'))
-app.use(express.static('/newpop'))
-app.use(express.static('/popcash'))
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', (request, response) => {
   // eslint-disable-next-line no-undef
   response.sendFile(path.resolve(__dirname, 'index.html'))
 })
-app.get('/exoclick', (request, response) => {
-  // eslint-disable-next-line no-undef
-  response.sendFile(path.resolve(__dirname, 'exoclick.html'))
-})
-app.get('/exoinpage', (request, response) => {
-  // eslint-disable-next-line no-undef
-  response.sendFile(path.resolve(__dirname, 'exoinpage.html'))
-})
+
 app.get('/adstera1', (request, response) => {
   // eslint-disable-next-line no-undef
   response.sendFile(path.resolve(__dirname, 'adstera1.html'))
@@ -36,14 +25,7 @@ app.get('/adsterra', (request, response) => {
   // eslint-disable-next-line no-undef
   response.sendFile(path.resolve(__dirname, 'adsterra.html'))
 })
-app.get('/popcash', (request, response) => {
-  // eslint-disable-next-line no-undef
-  response.sendFile(path.resolve(__dirname, 'popcash.html'))
-})
-app.get('/newpop', (request, response) => {
-  // eslint-disable-next-line no-undef
-  response.sendFile(path.resolve(__dirname, 'newpop.html'))
-})
+
 app.get('/direct', (request, response) => {
   // eslint-disable-next-line no-undef
   response.sendFile(path.resolve(__dirname, 'direct.html'))
